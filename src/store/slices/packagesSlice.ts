bfe4e66205f970034ea2f210e0acd0fd7ebe5c8b
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-05-13 12:23:06 +0530

    sd

--- src/store/slices/packagesSlice.ts
+++ src/store/slices/packagesSlice.ts
@@ -2,13 +2,10 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { AxiosError } from "axios";
 import { toast } from "react-hot-toast";
 import axiosInstance from "../../utils/axiosInstance";
-
-// Interfaces for API responses
 interface PackageRule {
   name: string;
   included: boolean;
 }
-
 interface Package {
   id: string;
   name: string;
@@ -16,36 +13,29 @@ interface Package {
   price: string;
   rules: PackageRule[];
 }
-
 interface ErrorResponse {
   message?: string;
 }
-
 export interface PackageState {
   packages: Package[];
   loading: boolean;
   error: string | null;
 }
-
-// Initial state
 const initialState: PackageState = {
   packages: [],
   loading: false,
   error: null,
 };
-
-// Async thunk for fetching all packages
 export const fetchAllPackages = createAsyncThunk(
   "package/fetchAllPackages",
-  async (_, { rejectWithValue }) => {
+  async (packageFor: string, { rejectWithValue }) => {
     try {
-      const promise = axiosInstance.get<Package[]>("/packages/v1/getAllPackages");
+      const promise = axiosInstance.get<Package[]>(`/packages/v1/getAllPackages?package_for=${packageFor}`);
       toast.promise(promise, {
         loading: "Fetching packages...",
         success: "Packages fetched successfully!",
         error: "Failed to fetch packages",
       });
-      
       const response = await promise;
       return response.data;
     } catch (error) {
@@ -55,8 +45,6 @@ export const fetchAllPackages = createAsyncThunk(
     }
   }
 );
-
-// Package slice
 const packageSlice = createSlice({
   name: "package",
   initialState,
@@ -67,7 +55,6 @@ const packageSlice = createSlice({
     },
   },
   extraReducers: (builder) => {
-    // Fetch all packages
     builder
       .addCase(fetchAllPackages.pending, (state) => {
         state.loading = true;
@@ -83,6 +70,5 @@ const packageSlice = createSlice({
       });
   },
 });
-
 export const { clearPackages } = packageSlice.actions;
 export default packageSlice.reducer;
