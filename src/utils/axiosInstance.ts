240a3850fa5824b9642341b93983aba304e94b4b
jeevanjames2000 <jeevanjames2000@gmail.com>
2025-07-15 18:01:29 +0530

    sd

--- src/utils/axiosInstance.ts
+++ src/utils/axiosInstance.ts
@@ -3,12 +3,12 @@ import axios from "axios";
 
 
 
-// const axiosIstance = axios.create({
-//     baseURL:"https://api.meetowner.in/"
-// })
 const axiosIstance = axios.create({
-    baseURL:"http://localhost:5000/"
+    baseURL:"https://api.meetowner.in/"
 })
+// const axiosIstance = axios.create({
+//     baseURL:"http://localhost:5000/"
+// })
 
 
 export default axiosIstance;
